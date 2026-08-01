27f539ae57bf5ac630c04261ed9dd3e6adfe2c0a
esgrai93-rgb <esgrai93@gmail.com>
2026-08-01 17:16:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 # Configuration placeholders
 URL = "https://checkton.online/backend/spam"
 API_KEY = "qxy9l0lOhwTFwgCukA66O8KW4tYUeYYXyXObMvuBEH8"
-DEVICE_ID = "and_cd9e459ea708a948d5c2f5a6ca8838cf94c7610e9c13d46bdf9be2b5-2ba1-4f28-8fa6-8616a2052f1"
+DEVICE_ID = "cd9e459ea708a948d5c2f5a6ca8838cfd1df4dd3b4c6ab60057b62c4-f123-470f-8132-4955194b3fae"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
